2e4e45e1a8879f203f0fcb00b18fa29db20d1b63
Katarzyna Kensy <zbierek@gmail.com>
2018-04-19 13:59:23 +0200

    Add else if in script.js

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ var a = 5;
 var b = 10;
 var value = (a * a) - (2 * a * b) - (b * b);
 
-if(value > 0) {
+  if(value > 0) {
   console.log("wynik dodatni");
-}
-else {
+} else if(value == 0) {
+  console.log("wynik jest równy zero");
+} else {
   console.log("wynik ujemny");
 }
